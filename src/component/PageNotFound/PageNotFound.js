ff79d6b5ee37d7a50f6b28024d1912c6f6a178a2
tausif40 <tausif6040@gmail.com>
2024-09-30 15:33:38 +0530

    page not found

--- src/component/PageNotFound/PageNotFound.js
+++ src/component/PageNotFound/PageNotFound.js
@@ -8,8 +8,8 @@ function PageNotFound() {
 
 	return (
 		<>
-			<div className='w-full h-screen flex flex-col items-center'>
-				<img src="./assets/img/404.svg" alt="Page Not Found" className='h-[75%]' />
+			<div className='w-full h-screen flex flex-col items-center border-t'>
+				<img src="./assets/img/404.svg" alt="Page Not Found" className='px-4 md:h-[75%]' />
 				<p className='mt-6 px-4 py-1 rounded-md font-light cursor-pointer text-xl text-primary gradient-btn' onClick={handleBackClick}>back...</p>
 			</div>
 		</>
